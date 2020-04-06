86b39a0aa3aace830982f90316e1346fe420cef1
Edwin Dominguez <edwindominguez@hotmail.com>
2020-04-06 13:24:28 -0500

    add request mapping to controller, edit port

--- src/main/java/com/donnatto/springbootbatch/controller/JobInvokerController.java
+++ src/main/java/com/donnatto/springbootbatch/controller/JobInvokerController.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +17,7 @@ public class JobInvokerController {
     @Autowired
     Job processJob;
 
+    @RequestMapping("/invokejob")
     public String handle() throws Exception {
 
         JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis())
